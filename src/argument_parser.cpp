e1f2011c9d2a0e4186d28a958ba4492a86a364d5
Matthis Kruse <s8makrus@stud.uni-saarland.de>
2019-10-29 11:11:10 +0100

    Implement stub partially.

--- src/argument_parser.cpp
+++ src/argument_parser.cpp
@@ -1,5 +1,8 @@
 #include <arguments_parser.hpp>
+#include <stream_lookup.hpp>
 #include <config.hpp>
+
+#include <cassert>
 
 namespace arguments
 {
@@ -11,6 +14,21 @@ void parse(const std::vector<std::string>& args)
   {
     if(v == "-h" || v == "-?" || v == "--help")
       config.print_help = true;
+    else
+    {
+      // must be a file or error
+      std::fstream file(v, std::ios::in);
+      if(!file)
+      {
+        // could not open file TODO
+        assert(false);
+      }
+      else
+      {
+        stream_lookup[v];
+        stream_lookup.drop(v);
+      }
+    }
   }
 }
 
