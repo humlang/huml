23da68b5794c5512b7ce2c449949a22470bbf0d5
Matthis Kruse <s8makrus@stud.uni-saarland.de>
2019-12-17 11:27:39 +0100

    Allow recursive calls in the ast_printer.
    We discussed about how to recursively call an ast_visitor as it is
    defined until the last commit. The key idea to recursive calls, since an
    ast_visitor in our case is just a lambda, is to basically add a
    "recursor" as argument, which should be the function itself. Thankfully
    to C++17, this is not painful anymore and can be just `auto recursor` in
    the parameter list of a lambda, which is just a syntactic sugar for
    templates.

--- include/ast_printer.hpp
+++ include/ast_printer.hpp
@@ -1,23 +1,30 @@
 #pragma once
 
+#include <functional>
+
 #include <ast.hpp>
 
 // https://en.cppreference.com/w/cpp/utility/variant/visit (16.10.19)
 template<class... Ts> struct overloaded : Ts... { using Ts::operator()...; };
 template<class... Ts> overloaded(Ts...) -> overloaded<Ts...>;
 
+// The recursor allows us to recursively call ast_printer_helper
 template<typename PrinterFn>
-inline static constexpr auto ast_printer = overloaded {
-  [](auto arg) { PrinterFn print;
+inline static constexpr auto ast_printer_helper = overloaded {
+  [](auto recursor, auto arg) { PrinterFn print;
     print("unknown argument");
   },
 
-  [](literal lit) { PrinterFn print;
+  [](auto recursor, literal lit) { PrinterFn print;
     print("literal token at " + lit.loc().to_string());
   },
 
-  [](identifier id) { PrinterFn print;
+  [](auto recursor, identifier id) { PrinterFn print;
     print("identifier token at " + id.loc().to_string());
   }
 };
 
+// The actual ast_printer now just needs to plug everything accordingly
+template<typename PrinterFn>
+inline static constexpr auto ast_printer = 
+			[](auto arg) { return ast_printer_helper<PrinterFn>(ast_printer_helper<PrinterFn>, arg); };
