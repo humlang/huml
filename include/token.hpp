58dddc22b4c3043559413ce06f2381a85f7b7733
xaver.fabian <s8xafabi@stud.uni-saarland.de>
2020-01-25 23:51:25 +0100

    changed behaviur of Assign token. Split into Assign := token with value 4 and another Equal token for just '='. Further I added a Semi token for ; and Colon token for :

--- include/token.hpp
+++ include/token.hpp
@@ -11,13 +11,16 @@ enum class token_kind : std::int8_t
     Identifier = 1,
     LiteralNumber = 2,
     Keyword = 3,
+    Assign = 4, // will be :=
     Point = '.',
+    Colon = ':',
+    Semi = ';',
     LBrace = '{',
     RBrace = '}',
     Plus = '+',
     Minus = '-',
     Asterisk = '*',
-    Assign = '=',
+    Equal = '=',
     Undef = 0,
     EndOfFile = EOF
 };
