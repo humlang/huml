80bdcb8f5adc46ecb766b599a33164bd47336c1b
Matthis Kruse <s8makrus@stud.uni-saarland.de>
2020-02-19 19:34:18 +0100

    Add basic implementation of arena.

--- include/arena.hpp
+++ include/arena.hpp
@@ -1,21 +1,95 @@
 #pragma once
 
+#include <memory>
 #include <vector>
 
-struct arena
+template<typename T>
+struct monotonic
 {
-  arena() : capacity(5000) // alloc 5kB
-          , buffer(capacity)
+public:
+  union monotonic_item
+  {
+  public:
+    monotonic_item *get_next_item() const { return next; }
+    void set_next_item(monotonic_item *n) { next = n; }
+ 
+    T *get_storage() { return reinterpret_cast<T *>(datum); }
+ 
+    static monotonic_item *storage_to_item(T *t)
+    {
+      monotonic_item *current_item = reinterpret_cast<monotonic_item *>(t);
+      return current_item;
+    }
+  private:
+    using StorageType = alignas(alignof(T)) char[sizeof(T)];
+ 
+    monotonic_item *next;
+    StorageType datum;
+  };
+
+  struct monotonic_arena
+  {
+  private:
+    std::unique_ptr<monotonic_item[]> storage;
+    std::unique_ptr<monotonic_arena> next;
+
+    monotonic_arena(std::size_t arena_size)
+      : storage(new monotonic_item[arena_size])
+    {
+      for(std::size_t i = 1; i < arena_size; i++)
+        storage[i - 1].set_next_item(&storage[i]);
+      storage[arena_size - 1].set_next_item(nullptr);
+    }
+
+    monotonic_item *get_storage() const { return storage.get(); }
+
+    void set_next_arena(std::unique_ptr<monotonic_arena> &&n)
+    {
+      assert(!next);
+ 
+      next.reset(n.release());
+    }
+  };
+
+public:
+  monotonic(std::size_t arena_size)
+      : arena_size(arena_size), arena(new monotonic_arena(arena_size)),
+        free_list(arena->get_storage())
   {  }
 
-  template<typename T, typename... Args>
-  T& alloc(Args&&... args);
+  template<typename... Args>
+  T *alloc(Args &&... args)
+  {
+    if(free_list == nullptr)
+    {
+      std::unique_ptr<monotonic_arena> new_arena(new monotonic_arena(arena_size));
+      new_arena->set_next_arena(std::move(arena));
+      arena.reset(new_arena.release());
+      free_list = arena->get_storage();
+    }
+ 
+    monotonic_item *current_item = free_list;
+    free_list = current_item->get_next_item();
+ 
+    T *result = current_item->get_storage();
+    new (result) T(std::forward<Args>(args)...);
+ 
+    return result;
+  }
 
-  template<typename T>
-  void destruct();
+  void free(T *t)
+  {
+    t->T::~T();
+ 
+    monotonic_item *current_item = monotonic_item::storage_to_item(t);
+ 
+    current_item->set_next_item(free_list);
+    free_list = current_item;
+  }
 
 private:
-  std::size_t capacity;
-  std::vector<unsigned char> buffer;
-};
+  std::size_t arena_size;
+  std::unique_ptr<monotonic_arena> arena;
+  monotonic_item *free_list;
+}; 
 
